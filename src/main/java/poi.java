5732a4f1457a7bfcca7bfd7e45ee2d2dbf48708a
wanglonglongs <wanglonglongiii@163.com>
2021-02-08 23:01:06 +0800

    03版excel

--- src/main/java/poi.java
+++ src/main/java/poi.java
@@ -1,3 +1,4 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -16,10 +17,10 @@ import java.util.Date;
  * @Version V1.0
  **/
 public class poi {
-
+    String PATH = "D:\\idea-projects\\lianxi\\PoiAndEaseExcle";
     @Test
-    public void poi () throws Exception {
-        String PATH = "D:\\idea-projects\\lianxi\\PoiAndEaseExcle";
+    public void poi07 () throws Exception {
+
 
         //创建工作簿 03 07 XSSFWorkbook()使用的对象不一样
         Workbook workbook = new XSSFWorkbook();
@@ -45,5 +46,32 @@ public class poi {
         //关闭流
         fileOutputStream.close();
         System.out.println("文件生成完毕");
+    }
+
+    @Test
+    public void poi03() throws Exception {
+        //创建工作簿
+        Workbook workbook = new HSSFWorkbook();
+        //工作页
+        Sheet sheet = workbook.createSheet("03版本的写入");
+        //创建行
+        Row row = sheet.createRow(0);
+        //单元格
+        Cell cell = row.createCell(0);
+        //赋值
+        cell.setCellValue("03版本写入");
+        //第一行第二列单元格
+        Row row1 = sheet.createRow(1);
+        Cell cell1 = row1.createCell(0);
+        cell1.setCellValue(new Date());
+        //文件上传路径
+        FileOutputStream fileOutputStream = new FileOutputStream(PATH + "//03版本excel.xls");
+        //写入
+        workbook.write(fileOutputStream);
+
+        //关闭流
+        fileOutputStream.close();
+        //输出
+        System.out.println("03版本写入excel,yes");
     }
 }
