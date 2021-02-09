841a8eaaa137f46fb7a040fd5fa52a39ca4e3171
wanglonglongs <wanglonglongiii@163.com>
2021-02-09 18:07:26 +0800

    读取不同文件注意

---
+++ src/main/java/readPoi.java
@@ -0,0 +1,65 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @ClassName readPoi
+ * @Description: TODO
+ * @Author wanglonglong
+ * @Date 2021/2/9
+ * @Version V1.0
+ **/
+public class readPoi {
+    String PATH = "D:\\idea-projects\\lianxi\\PoiAndEaseExcle";
+    //读取03版
+    @Test
+    public void read03() throws IOException {
+
+
+        //获取文件流
+        FileInputStream fileInputStream = new FileInputStream(PATH + "//03版本excel.xls");
+
+        //创建一个工作簿。使用excel能操作的功能这里都有
+        Workbook workbook = new HSSFWorkbook(fileInputStream);
+
+        //获取表（得到表）
+        Sheet sheet = workbook.getSheetAt(0);
+        //行得到行）
+        Row row = sheet.getRow(0);
+        //列》= row.getCell(0);
+        //注意获取不同类型的值
+        Cell cell = row.getCell(0);
+
+        System.out.println(cell.getStringCellValue());
+        //关闭流
+        fileInputStream.close();
+    }
+    //读取07版
+    @Test
+    public void read07() throws Exception {
+        //获取流
+        FileInputStream fileInputStream = new FileInputStream(PATH + "//poi.xlsx");
+        //工作簿
+        Workbook workbook = new XSSFWorkbook(fileInputStream);
+        //查找页
+        Sheet sheetAt = workbook.getSheetAt(0);
+        //查找行
+        Row row = sheetAt.getRow(1);
+        //查找列
+        Cell cell = row.getCell(0);
+        //类型为DATE
+        System.out.println(cell.getDateCellValue());
+
+        //关闭流
+        fileInputStream.close();
+    }
+    //计算公式
+
+}
